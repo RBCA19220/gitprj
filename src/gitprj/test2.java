5fbccfff66a48e147a25347af2cbbcaed03eccca
shive <shive@DESKTOP-H7QUKCV>
2021-02-25 01:29:07 +0530

    check1

---
+++ src/gitprj/test2.java
@@ -0,0 +1,12 @@
+package gitprj;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello1");
+		System.out.println("bro, how are you ?");
+
+	}
+
+}
